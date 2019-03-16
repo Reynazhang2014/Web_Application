3a1cbe9fc26fc175e35a9d9d15506cfcf6120aa8
Mina_Abad <mbaghgar@uci.edu>
2019-03-16 01:07:42 -0700

    Mina's first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+app=Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+# db = SQLAlchemy(app)
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
+
+# # Save references to each table
+# Samples_Metadata = Base.classes.sample_metadata
+# Samples = Base.classes.samples
+def loadData():
+    df=pd.read_csv("db/pricepersqft.csv") 
+    df=df.set_index(["City Code"
+                    ,"City"
+                    ,"Lat","Lon"
+                    ,"Metro"
+                    ,"County"
+                    ,"State"
+                    ,"Population Rank"])
+    # statck the columns of year and month into one column 
+    # and add a column to indicate month-year                
+    df=df.stack().reset_index()
+    df=df.rename(columns={"level_8":"month_year",0:"Price_Persq","Population Rank":"PopulationRank"})
+    # separate month-year to two columns
+    map_month_to_num = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+    df["month"]=df["month_year"].apply(lambda x: x[:3]).map(map_month_to_num)
+    df["Year"]=df["month_year"].apply(lambda x: "20"+ x[4:]).astype(int)
+    # filter out the year before 2012
+    df= df[df['Year']>=2012]
+    #df.fillna(0,inplace = True)
+    df=df.drop(["month_year"],axis=1)
+    return df
+
+df=loadData()   
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+@app.route("/pricepersqft",methods=['GET','POST'])
+def filterPricePerSqft():
+    print(request.data)
+    tempdf=df
+    if request.data:
+        filterdict = json.loads(request.data)
+        for k,v in filterdict.items():
+            tempdf=tempdf[tempdf[k].astype(str).str.upper()==v.upper()]
+        return tempdf.to_json(orient='records')
+    return jsonify([])
+
+@app.route("/yearlyprice")
+def yearlyprice():
+    ave_yearly_price=df.groupby(["City","Year"])["Price_Persq"].mean().reset_index().rename(columns={"Price_Persq":"AvePricePersq"}).sort_values(by="AvePricePersq")
+    return(ave_yearly_price.to_json(orient='records'))
+
+
+ 
+if __name__ == "__main__":
+    app.run(debug =True)
---
+++ static/js/app.js
@@ -0,0 +1,165 @@
+let price=[];
+let avePrice=[];
+// Keep Track of all filters
+var priceFilters = {"Year":"2017","City":"Irvine"};
+console.log(priceFilters);
+
+// function getData(){
+//     console.log("reading data")
+//     let price_url="/pricepersqft";
+//     d3.json(price_url).then((data,error)=> {
+//         console.log(error);
+//         price=data;
+//         console.log(price);
+//     })
+//     let avePrice_url="/yearlyprice"; 
+//     d3.json(avePrice_url).then((data,error) => {
+//         //console.log(error);
+//         avePrice=data;
+//         //console.log(avePrice);
+//     })   
+
+ 
+// };
+
+
+
+function plots(data){
+    console.log(data);
+
+}
+
+
+
+function getData() {
+    console.log("running getData")
+    
+    //console.log(filteredData);
+    d3.json('/pricepersqft', {method: 'POST', body: JSON.stringify(priceFilters)})
+    .then(data => plots(data));
+    //getData();
+
+
+
+    // // Loop through all of the filters and keep any data that
+    // // matches the filter values
+    // Object.entries(priceFilters).forEach(([key, value]) => {
+    //   filteredData = filteredData.filter(row => row[key] === value);
+    // });
+    // console.log(filteredData);
+    // // Finally, rebuild the table using the filtered Data ???
+    // //call plot functions
+    // plots( filteredData);
+    
+  }
+
+
+  
+  function updatePriceFilters() {
+    console.log("running updatePriceFilter");
+    // Save the element, value, and id of the filter that was changed
+    var changedElement = d3.select(this).select("input");
+    var elementValue = changedElement.property("value");
+    var filterId = changedElement.attr("id");
+  
+    // If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object
+    if (elementValue) {
+      priceFilters[filterId] = elementValue;
+    }
+    else {
+      delete priceFilters[filterId];
+    }
+  
+    console.log(priceFilters);
+    }
+
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".price-filter").on("change", updatePriceFilters);
+d3.selectAll(".btn-filter").on("click",getData);
+
+getData();
+
+
+
+//{
+//     d3.event.preventDefault();
+//     console.log(priceFilters);
+//     filterJS(priceFilters);
+
+// });
+// from data.js
+// allData=[];
+// d3.csv("price.csv").then((data,error) => {
+//   //var allData = data;
+//   console.log(data);
+// });
+
+
+// // get table references
+// var tbody = d3.select("tbody");
+
+// function buildTable(data) {
+//   // First, clear out any existing data
+//   tbody.html("");
+
+//   // Next, loop through each object in the data
+//   // and append a row and cells for each value in the row
+//   data.forEach((dataRow) => {
+//     // Append a row to the table body
+//     var row = tbody.append("tr");
+
+//     // Loop through each field in the dataRow and add
+//     // each value as a table cell (td)
+//     Object.values(dataRow).forEach((val) => {
+//       var cell = row.append("td");
+//       cell.text(val);
+//     });
+//   });
+// }
+
+// // Keep Track of all filters
+// var filters = {};
+
+// function updateFilters() {
+
+//   // Save the element, value, and id of the filter that was changed
+//   var changedElement = d3.select(this).select("input");
+//   var elementValue = changedElement.property("value");
+//   var filterId = changedElement.attr("id");
+
+//   // If a filter value was entered then add that filterId and value
+//   // to the filters list. Otherwise, clear that filter from the filters object
+//   if (elementValue) {
+//     filters[filterId] = elementValue;
+//   }
+//   else {
+//     delete filters[filterId];
+//   }
+
+//   // Call function to apply all filters and rebuild the table
+//   filterTable();
+
+// }
+
+// function filterTable() {
+
+//   // Set the filteredData to the tableData
+//   let filteredData = tableData;
+
+//   // Loop through all of the filters and keep any data that
+//   // matches the filter values
+//   Object.entries(filters).forEach(([key, value]) => {
+//     filteredData = filteredData.filter(row => row[key] === value);
+//   });
+
+//   // Finally, rebuild the table using the filtered Data
+//   buildTable(filteredData);
+// }
+
+// // Attach an event to listen for changes to each filter
+// d3.selectAll(".filter").on("change", updateFilters);
+
+// // Build the table when the page loads
+// buildTable(tableData);
